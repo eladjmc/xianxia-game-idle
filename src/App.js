253b20c75359e92f2875022edb1e3e183f499852
Your Name <eladjmc88@gmail.com>
2023-09-22 16:50:28 +0300

    added the basic game window and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar/Navbar";
+import MainPage from "./pages/MainPage/MainPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="game-main-container">
+      <main className="game-window">
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              {/* Add more routes here */}
+            </Routes>
+          </div>
+        </Router>
+      </main>
     </div>
   );
 }
