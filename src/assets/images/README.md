4e48b906ff0f5063e2066cffa31e344d5e2fdd93
Your Name <eladjmc88@gmail.com>
2023-09-22 16:25:58 +0300

    added basic folder structure

---
+++ src/assets/images/README.md
@@ -0,0 +1,1 @@
+# project images in this Directory
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MainPage.scss'
+const MainPage = () => {
+  return (
+    <div>
+      this is the main page
+    </div>
+  )
+}
+
+export default MainPage
