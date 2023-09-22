d2f15a972ec8ff57fe3f1988f1eb17a22bb9a012
Your Name <eladjmc88@gmail.com>
2023-09-22 17:22:34 +0300

    decided on some colors

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import './Navbar.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';  
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="navbar-links">
+        <Link to="/">Cultivate</Link>
+        {/* Add more links as you create more pages */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
